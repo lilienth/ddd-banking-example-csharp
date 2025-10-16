1737f5953ccbbfb6e99cecf88b5c252a6eac07a5
Henning Schwentner <hs@wps.de>
2025-10-16 16:20:30 +0200

    Continue to port level 3 from Java

---
+++ 2 Hands-on Bounded Contexts/src/Banking/Crediting/CreditCustomer.cs
@@ -0,0 +1,52 @@
+namespace Banking.Crediting;
+
+public class CreditCustomer {
+	private String firstName;
+	private String familyName;
+	private DateOnly dateOfBirth;
+	private int customerNumber;
+	private IList<CreditAccount> accountList;
+	private IList<Credit> creditList;
+
+	public CreditCustomer(String firstName, String familyName, LocalDate dateOfBirth, int customerNumber)
+    {
+		super();
+		this.firstName = firstName;
+		this.familyName = familyName;
+		this.dateOfBirth = dateOfBirth;
+		this.customerNumber = customerNumber;
+		accountList = new List<CreditAccount>();
+		creditList = new List<Credit>();
+	}
+
+	public String getFirstName()
+    {
+		return firstName;
+	}
+
+	public String getFamilyName()
+    {
+		return familyName;
+	}
+
+	public LocalDate getDateOfBirth()
+    {
+		return dateOfBirth;
+	}
+
+	public int getCustomerNumber()
+    {
+		return customerNumber;
+	}
+
+	public List<CreditAccount> getAccountList()
+    {
+		return accountList;
+	}
+
+	public List<Credit> getCreditList()
+    {
+		return creditList;
+	}
+
+}
---
+++ 3 Hands-on Value Objects/src/Banking/Crediting/CreditCustomer.cs
@@ -0,0 +1,53 @@
+namespace Banking.Crediting;
+
+using Banking.SharedKernel;
+
+public class CreditCustomer {
+	private String firstName;
+	private String familyName;
+	private LocalDate dateOfBirth;
+	private CustomerNumber customerNumber;
+	private IList<CreditAccount> accountList;
+	private IList<Credit> creditList;
+
+	public CreditCustomer(CustomerNumber customerNumber, String firstName, String familyName, LocalDate dateOfBirth) {
+		super();
+		this.firstName = firstName;
+		this.familyName = familyName;
+		this.dateOfBirth = dateOfBirth;
+		this.customerNumber = customerNumber;
+		accountList = new List<>();
+		creditList = new List<>();
+	}
+
+	public String getFirstName()
+    {
+		return firstName;
+	}
+
+	public String getFamilyName()
+    {
+		return familyName;
+	}
+
+	public DateOnly getDateOfBirth()
+    {
+		return dateOfBirth;
+	}
+
+	public CustomerNumber getCustomerNumber()
+    {
+		return customerNumber;
+	}
+
+	public List<CreditAccount> getAccountList()
+    {
+		return accountList;
+	}
+
+	public List<Credit> getCreditList()
+    {
+		return creditList;
+	}
+
+}
