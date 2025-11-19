542b9c0f965ba19b27a5802ef143cee329ff1534
Henning Schwentner <hs@wps.de>
2025-11-19 10:22:58 +0100

    Finish port of "2 Hands-on Bounded Contexts"

--- 2 Hands-on Bounded Contexts/tests/Banking.Test/Credit/CreditCustomerTest.cs
+++ 2 Hands-on Bounded Contexts/tests/Banking.Test/Credit/CreditCustomerTest.cs
@@ -1,18 +1,19 @@
 using Banking.Accounting;
-using Banking.SharedKernel;
 
 namespace Banking.Crediting;
 
 public class CreditCustomerTest
 {
-	[Fact]
-	void testCustomerConstruction()
+    [Fact]
+	void TestCustomerConstruction()
     {
-		CreditCustomer customer = new CreditCustomer(CustomerNumber.Of(1), "Carola", "Lilienthal", new DateOnly(1967, 9, 11));
+		CreditCustomer customer = new CreditCustomer("Carola", "Lilienthal", new DateOnly(1967, 9, 11), 11);
 		Assert.Equal("Carola", customer.getFirstName());
 		Assert.Equal("Lilienthal", customer.getFamilyName());
 		Assert.Equal(new DateOnly(1967, 9, 11), customer.getDateOfBirth());
-		Assert.NotNull(customer.getCustomerNumber());
+		Assert.Equal(11, customer.getCustomerNumber());
 		Assert.NotNull(customer.getAccountList());
+		Assert.NotNull(customer.getCreditList());
 	}
+
 }
